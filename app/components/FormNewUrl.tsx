65c169fb7a6674df468ff90696b5955dbcb65d7e
tuiger10152025 <tuiger10152025@gmail.com>
2024-06-03 08:52:11 +0000

    new

--- app/components/FormNewUrl.tsx
+++ app/components/FormNewUrl.tsx
@@ -34,11 +34,11 @@ export default function FormNewUrl() {
             const response = await res.json() as resUrl
             console.log(response);
             if (res.status !== 200) {
-                setResult("")
-                setLoading(false)
                 setError(response.msg)
                 throw new Error(response.msg)
             }
+            setResult("")
+            setLoading(false)
             setResult(response.newurl)
 
 
